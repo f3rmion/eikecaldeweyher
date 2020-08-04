4ba35e7085bf9bfc926e1d2a6247107840c2b04f
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-08-04 17:05:25 +0200

    Prettify posts view for research index.

--- resources/views/public/research/index.blade.php
+++ resources/views/public/research/index.blade.php
@@ -20,9 +20,20 @@
 				</a>
 				@endif
 			</div>
-			<a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+			<div class="text-gray-600 text-xs mt-2">
+				<button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150">
+				  {{ $post->category->name }}
+				</button>
+			</div>
+			<a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
 				{{ $post->title }}
 			</a>
+			<div class="text-indigo-600 text-xs mt-2 font-semibold">
+				Tags:
+				<button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 text-xs leading-4 font-medium rounded text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
+				  {{ $post->tags->pluck('name')->implode(', ') }}
+				</button>
+			</div>
 			<div class="text-gray-600 text-xs mt-1">
 			{{ $post->getAuthors() }}	
 			</div>
