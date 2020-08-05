2367ad6c57b3dabb1037b38d5e16082523e3f4a6
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-08-05 14:40:18 +0200

    Hide body in posts index view.

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -19,9 +19,6 @@
               Title
             </th>
             <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-              Body
-            </th>
-            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
               Author
             </th>
             <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
@@ -43,9 +40,6 @@
 			  <tr class="bg-white">
 				<td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
 				  {!! \Illuminate\Support\Str::limit($post->title, 30, '...') !!}
-				</td>
-				<td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-				  {!! $post->trixRichText()->where('field', 'body')->first()->content !!}
 				</td>
 				<td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
 				  {{ $post->user->name }}
