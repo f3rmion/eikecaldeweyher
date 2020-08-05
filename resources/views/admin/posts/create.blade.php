69215f87a3b994e6c6eefdd191db6d58b625a86c
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-08-05 14:39:18 +0200

    Hide attachment button in wysiwyg editor.

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -16,7 +16,7 @@
 
         <div class="col-span-4">
 			<label for="Authors" class="block text-sm font-medium leading-5 text-gray-700">Body</label>
-			@trix(\App\Post::class, 'body')
+			@trix(\App\Post::class, 'body', ['hideButtonIcons' => 'attach'])
 		</div>
 		
 		<div class="col-span-4">
