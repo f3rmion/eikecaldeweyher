c4ed9f59cb11b6a9de310b4c920fc2d336e8159b
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-08-11 10:59:35 +0200

    Add imprint view.

---
+++ resources/views/imprint.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container p-4 mx-auto">
+	<h1>Imprint</h1>
+	<div class="w-1/2 mt-8 bg-gray-100 leading-6 p-4">
+		Eike Caldeweyher<br>
+		Am Köppekreuz 11<br>
+		53225 Bonn<br>
+		Germany<br><br>
+		Contact: hello at eikecaldeweyher.de
+	</div>
+</div>
+@endsection
