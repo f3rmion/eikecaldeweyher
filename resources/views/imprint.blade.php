b840fb888b78f0dc62269148da8f204bfb41be31
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-08-11 11:01:48 +0200

    Improve mobile view for imprint.

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container p-4 mx-auto">
 	<h1>Imprint</h1>
-	<div class="w-1/2 mt-8 bg-gray-100 leading-6 p-4">
+	<div class="w-full lg:w-1/2 mt-8 bg-gray-100 leading-6 p-4">
 		Eike Caldeweyher<br>
 		Am Köppekreuz 11<br>
 		53225 Bonn<br>
