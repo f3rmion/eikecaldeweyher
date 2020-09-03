3d7486fb840d56b4f913cf3d10c57c7019b7eab0
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-09-03 19:35:25 +0200

    Update imprint.

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -5,9 +5,9 @@
 	<h1>Imprint</h1>
 	<div class="w-full lg:w-1/2 mt-8 bg-gray-100 leading-6 p-4">
 		Eike Caldeweyher<br>
-		Am Köppekreuz 11<br>
-		53225 Bonn<br>
-		Germany<br><br>
+		Viktoriagatan 34<br>
+		411 25 Gothenburg<br>
+		Sweden<br><br>
 		Contact: hello at eikecaldeweyher.de
 	</div>
 </div>
