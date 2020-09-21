2d9f4a00eb8c2f3701a1ad43d8bc27034754483d
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-09-21 17:01:22 +0200

    Clean up h2 on research show.

--- resources/views/public/research/show.blade.php
+++ resources/views/public/research/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="container mx-auto px-4">
-		<h2 class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
+		<h2 class="mb-8 mt-4 text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
 			<a href="{{ route('research.index') }}">Research and Development /</a>
 		</h2>		
 		<div class="post-details pb-12 flex flex-col lg:flex-row">
