2759ed928ef10a9ffbb40fd03a7e25171b44b863
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-11-08 01:09:27 +0100

    Use hashid instead of title.

--- resources/views/public/research/show.blade.php
+++ resources/views/public/research/show.blade.php
@@ -28,7 +28,7 @@
 				<svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 				  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
 				</svg>
-				<a href="{{ route('research.show', $post) }}" aria-current="page" class="text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out">{{ $post->title}}</a>
+				<a href="{{ route('research.show', $post) }}" aria-current="page" class="text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out">{{ $post->hashid()}}</a>
 			  </div>
 			</li>
 		  </ol>
