9fb521b89edf28721eea521c734f3f73571d42ca
Eike Caldeweyher <hello@eikecaldeweyher.de>
2020-11-08 01:01:25 +0100

    Add breadcrumbs to research.

--- resources/views/public/research/index.blade.php
+++ resources/views/public/research/index.blade.php
@@ -2,7 +2,29 @@
 
 @section('content')
 <div class="container mx-auto pl-4 lg:p-4 lg:p-0">
-	<h2 class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Research and Development</h2>		
+	<nav class="flex mb-8" aria-label="Breadcrumb">
+	  <ol class="bg-white rounded-md shadow px-6 flex space-x-4">
+		<li class="flex">
+		  <div class="flex items-center">
+			<a href="{{ route('welcome') }}" class="text-gray-400 hover:text-gray-500">
+			  <!-- Heroicon name: home -->
+			  <svg class="flex-shrink-0 h-5 w-5 transition duration-150 ease-in-out" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+			  </svg>
+			</a>
+			<span class="sr-only">Home</span>
+		  </div>
+		</li>
+		<li class="flex">
+		  <div class="flex items-center space-x-4">
+			<svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+			  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+			</svg>
+			<a href="{{ route('research.index') }}" class="text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out">Research</a>
+		  </div>
+		</li>
+	  </ol>
+	</nav>
 	<div class="posts text-sm w-full lg:pl-0 pr-16 lg:pr-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 row-gap-10 col-gap-12 pb-16">
 	@foreach ($posts as $post)
 		<div class="post mt-8">
